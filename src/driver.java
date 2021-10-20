dcd9045306936546993b0d0252d75ba255bd1f3e
Aboubakar <aboubakarbarry572@gmail.com>
2021-10-20 15:24:30 -0400

    added speaker z

--- src/driver.java
+++ src/driver.java
@@ -6,7 +6,7 @@ public class driver {
     public static void main(String[] args) {
 
         Speaker x = new Speaker("John", "Doe", "Dr.", "Hershey Medical");
-
+        speaker z = new speaker();
         System.out.println(x.toString());
 
     }
